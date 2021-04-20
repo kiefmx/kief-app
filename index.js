33a7194f0f4f17bef9da59af3880f69daea03acc
Arturo Vázquez <arturo@yei3.com>
2021-04-20 15:24:10 -0500

    check .env

--- index.js
+++ index.js
@@ -38,6 +38,7 @@ if (process.env.NODE_ENV === 'production') {
 
 (async function () {
   try {
+    console.log(process.env.NODE_ENV);
     console.log(process.env.MONGO_URI);
     await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
