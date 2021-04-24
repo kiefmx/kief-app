00aada9eeaa061406f2e6f67419de27c905737b0
Arturo Vázquez <arturo@yei3.com>
2021-04-23 22:07:38 -0500

    Set PostType

--- client/src/utils/postTypeOptionList.js
+++ client/src/utils/postTypeOptionList.js
@@ -4,13 +4,16 @@ import FlowersoIcon from '@material-ui/icons/LocalFlorist';
 import SmileIcon from '@material-ui/icons/Face';
 import ConsumeIcon from '@material-ui/icons/Whatshot';
 import MedicalIcon from '@material-ui/icons/LocalHospital';
+import SelfCultiveIcon from '@material-ui/icons/Spa';
 
 
 export const getPostTypeOptionList = () => {  
-  return [ {label: "Info", value: 1, icon: InfoIcon},
-  {label: "Calendario", value: 2, icon: CalendarIcon}, 
-  {label: "Flores", value: 3, icon: FlowersoIcon}, 
-  {label: "Fun", value: 4, icon: SmileIcon}, 
-  {label: "ConsumoResponsable", value: 5, icon: ConsumeIcon},
-  {label: "Medicinal", value: 6, icon: MedicalIcon}];
+  return [ 
+    {label: "Noticias", value: 1, icon: InfoIcon, color:"#344444"},
+    {label: "Calendario", value: 2, icon: CalendarIcon}, 
+    {label: "Flores", value: 3, icon: FlowersoIcon},   
+    {label: "ConsumoResponsable", value: 4, icon: ConsumeIcon},
+    {label: "Medicinal", value: 5, icon: MedicalIcon},
+    {label: "Autocultivo", value: 6, icon: SelfCultiveIcon},
+  ];
 };
